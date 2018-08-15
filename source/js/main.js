d11c4a61cec3fd274d24cba662bb600affe9a7e6
william <william810212@gmail.com>
2018-08-15 20:13:09 +0800

    new project Airline-CI

---
+++ source/js/main.js
@@ -0,0 +1,28 @@
+// navbar
+$(window).scroll(function(){
+  var scrollRoll = $(this).scrollTop();
+  var carouselH = $("#myCarousel").height();
+  if(scrollRoll>carouselH){
+      $("#section_nav").addClass("fixed-top");
+  }else{
+      $("#section_nav").removeClass("fixed-top");
+  }
+})
+
+// gotop
+$(function(){
+  $("body").append("<a href='#' id='gotop'></a>");
+
+  $("#gotop").click(function(){
+      $("html,body").animate({ scrollTop: 0 }, 300);
+      return false;
+  })
+
+  $(window).scroll(function() {
+    if( $(this).scrollTop()>200 ){
+      $("#gotop").fadeIn(400);
+    } else{
+      $("#gotop").fadeOut(400);
+    }
+  });
+});
